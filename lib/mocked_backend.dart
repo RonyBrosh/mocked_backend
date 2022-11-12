b79f79315a7f8bf0140136d85f1d434f5268d8cc
Ron Brosh <63312304+ron-brosh@users.noreply.github.com>
2022-11-12 10:25:02 +0200

    Added scenario for better readability

--- lib/mocked_backend.dart
+++ lib/mocked_backend.dart
@@ -3,3 +3,4 @@ library mocked_backend;
 export 'package:mocked_backend/src/mocked_backend_interceptor.dart';
 export 'package:mocked_backend/src/model/mocked_request_handler.dart';
 export 'package:mocked_backend/src/model/request_matcher.dart';
+export 'package:mocked_backend/src/model/scenario.dart';
