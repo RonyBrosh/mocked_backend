a621d73796d196bfa848f2496d9c9e276f489d4e
Ron Brosh <63312304+ron-brosh@users.noreply.github.com>
2023-07-20 19:24:54 +0100

    updated example app

--- example/lib/src/domain/api/deezer_api.g.dart
+++ example/lib/src/domain/api/deezer_api.g.dart
@@ -19,11 +19,11 @@ class _DeezerApi implements DeezerApi {
   String? baseUrl;
 
   @override
-  Future<DeezerTracksResponse> getTracksForArtist(artist) async {
+  Future<DeezerTracksResponse> getTracksForArtist(String artist) async {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{r'artist': artist};
     final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
     final _result = await _dio.fetch<Map<String, dynamic>>(
         _setStreamType<DeezerTracksResponse>(Options(
       method: 'GET',
@@ -36,7 +36,11 @@ class _DeezerApi implements DeezerApi {
               queryParameters: queryParameters,
               data: _data,
             )
-            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
     final value = DeezerTracksResponse.fromJson(_result.data!);
     return value;
   }
@@ -52,5 +56,22 @@ class _DeezerApi implements DeezerApi {
       }
     }
     return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
   }
 }
--- example/lib/src/domain/api/model/deezer_tracks_response.freezed.dart
+++ example/lib/src/domain/api/model/deezer_tracks_response.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'deezer_tracks_response.dart';
 
@@ -106,6 +106,7 @@ class _$_DeezerTracksResponse implements _DeezerTracksResponse {
   final List<DeezerTrack> _data;
   @override
   List<DeezerTrack> get data {
+    if (_data is EqualUnmodifiableListView) return _data;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_data);
   }
